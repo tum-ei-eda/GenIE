1b4dc1e4bee124de34de658572e092e963de935b
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-02-20 21:39:22 +0100

    docker fix

--- genie/container.py
+++ genie/container.py
@@ -226,4 +226,5 @@ def run_in_container(
     info("Running containerized command:")
     print(shlex.join(cmd))
 
-    os.execlp(CONTAINER_ENGINE, *cmd)
+    # os.execlp(CONTAINER_ENGINE, *cmd)
+    subprocess.call(cmd)
